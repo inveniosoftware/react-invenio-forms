ac1ebf62bc324f0dc2f2f6b27fe8084f88ae6a9a
Pal Kerecsenyi <pal@p.kerecs.com>
2025-07-25 14:08:40 +0200

    chore: fix incorrect import

--- src/lib/forms/widgets/text/NumberInput.js
+++ src/lib/forms/widgets/text/NumberInput.js
@@ -3,7 +3,7 @@ import {
   createDynamicOverridableComponent,
   createShowHideComponent,
 } from "../../../utils";
-import InputComponent from "./Input";
+import { InputComponent } from "./Input";
 
 const _NumberInputComponent = (props) => <InputComponent {...props} type="number" />;
 export const NumberInputComponent = createShowHideComponent(_NumberInputComponent);
