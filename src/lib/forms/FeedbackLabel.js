0754c8b9cf512c6f360f3cd2773ff0829dee58f3
Maira Salazar <mpssalazar@gmail.com>
2026-09-03 09:10:33 +0200

    refactor: remove overridable
    Instead, consider the origin when rendering the icon, if origin is
    passed.

--- src/lib/forms/FeedbackLabel.js
+++ src/lib/forms/FeedbackLabel.js
@@ -7,10 +7,13 @@ import _get from "lodash/get";
 import isEmpty from "lodash/isEmpty";
 import React, { Component } from "react";
 import { Label, Icon } from "semantic-ui-react";
-import Overridable from "react-overridable";
 import { InvenioPopup } from "../elements/accessibility";
 import PropTypes from "prop-types";
 import { Field } from "formik";
+
+const ORIGIN_ICONS = {
+  orcha: "fire blue",
+};
 
 export class FeedbackLabel extends Component {
   getAllErrSubPaths = (obj, prev = "") => {
@@ -78,24 +81,16 @@ export class FeedbackLabel extends Component {
     return (
       <Label pointing={pointing} className={isError ? "prompt" : error.severity}>
         {hasSeverity && (
-          <Overridable
-            id="ReactInvenioForms.FeedbackLabel.popup"
-            fieldPath={fieldPath}
-            error={error}
-            icon={icon}
-            isError={isError}
-          >
-            <InvenioPopup
-              popupId={`invenio-form-feedback-error-${fieldPath}`}
-              ariaLabel="Form field feedback error"
-              trigger={<Icon name={icon} />}
-              // Rule descriptions can contain HTML to link to a page with more details about the rule.
-              // This field is sanitized in the backend with SanitizedHTML.
-              content={<span dangerouslySetInnerHTML={{ __html: error.description }} />}
-              position="top center"
-              hoverable
-            />
-          </Overridable>
+          <InvenioPopup
+            popupId={`invenio-form-feedback-error-${fieldPath}`}
+            ariaLabel="Form field feedback error"
+            trigger={<Icon name={ORIGIN_ICONS[error.origin] || icon} />}
+            // Rule descriptions can contain HTML to link to a page with more details about the rule.
+            // This field is sanitized in the backend with SanitizedHTML.
+            content={<span dangerouslySetInnerHTML={{ __html: error.description }} />}
+            position="top center"
+            hoverable
+          />
         )}
         {/* Display either the error text or the severity message */}
         {errMessage}
